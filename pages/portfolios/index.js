bb47f4412d2af7d57fa963dcbd20d12cb80dd222
jyotiprakash-m <teachmeonline365@gmail.com>
2021-06-24 22:38:22 +0530

    fetch all portfolos

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios'
+import PortfolioCard from '@/components/portfolios/PortfolioCard';
 
 const apiCall = () => {
     return new Promise((res, rej) => {
@@ -10,12 +12,33 @@ const apiCall = () => {
     })
 }
 
-const Portfolios = (props) => {
+// Fetch Portfolios
+const fetchPortfolios = () => {
+    const query = `
+      query Portfolios {
+        portfolios {
+          _id,
+          title,
+          company,
+          companyWebsite
+          location
+          jobTitle
+          description
+          startDate
+          endDate
+        }
+      }`;
+    return axios.post('http://localhost:3000/graphql', { query })
+        .then(({ data: graph }) => graph.data)
+        .then(data => data.portfolios)
+}
+
+
+const Portfolios = ({ portfolios }) => {
 
     return (
         <>
             <div className="container">
-                {props.testingData}
                 <section className="section-title">
                     <div className="px-2">
                         <div className="pt-5 pb-4">
@@ -25,42 +48,13 @@ const Portfolios = (props) => {
                 </section>
                 <section className="pb-5">
                     <div className="row">
-                        <div className="col-md-4">
-                            <div className="card subtle-shadow no-border">
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                                    <p className="card-text fs-2">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                </div>
-                                <div className="card-footer no-border">
-                                    <small className="text-muted">Last updated 3 mins ago</small>
-                                </div>
+                        {portfolios.map(portfolio =>
+
+                            <div key={portfolio._id} className="col-md-4">
+                                <PortfolioCard portfolio={portfolio} />
                             </div>
-                        </div>
-                        <div className="col-md-4">
-                            <div className="card subtle-shadow no-border">
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                                    <p className="card-text fs-2 ">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                </div>
-                                <div className="card-footer no-border">
-                                    <small className="text-muted">Last updated 3 mins ago</small>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4">
-                            <div className="card subtle-shadow no-border">
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                                    <p className="card-text fs-2 ">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                </div>
-                                <div className="card-footer no-border">
-                                    <small className="text-muted">Last updated 3 mins ago</small>
-                                </div>
-                            </div>
-                        </div>
+                        )}
+
                     </div>
                 </section>
             </div>
@@ -70,8 +64,8 @@ const Portfolios = (props) => {
 
 Portfolios.getInitialProps = async () => {
 
-    const data = await apiCall();
-    return { ...data }
+    const portfolios = await fetchPortfolios();
+    return { portfolios }
 }
 
 export default Portfolios;
