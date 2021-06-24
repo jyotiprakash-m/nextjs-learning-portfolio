d432228156fc7f3c18c3cc2ad5009e23546b3c33
jyotiprakash-m <teachmeonline365@gmail.com>
2021-06-24 08:10:53 +0530

    more pages added

--- pages/forum/categories/index.js
+++ pages/forum/categories/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Navbar from "../../components/shared/Navbar"
-import Hero from "../../components/shared/Hero"
+import Navbar from "../../../components/shared/Navbar"
+import Hero from "../../../components/shared/Hero"
 
 
 const ForumCategories = () => {
